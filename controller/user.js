68698103d79b7e848ed1d87703a00e0185db5d25
ArchanjoJr <archanjojr@my.com>
2019-06-04 23:17:01 -0300

    feature: add both post and delete

--- controller/user.js
+++ controller/user.js
@@ -19,10 +19,38 @@ module.exports.getUserById = (request, response) => {
     const { userId } = request.params;
     const query = db.format(rawString, userId);
     db.query(query, (err, results) => {
-      if (err) throw err
-      return response.json(results);
+      if (err) throw err;
+      return response.json(results[0]);
     });
   } catch(error) {
+    return response.status(505).json({error});
+  }
+}
+
+module.exports.createUser = (request, response) => {
+  try {
+    const rawString = 'insert into usuario(email, pass) value (?,?);';
+    const { email, password } = request.body;
+    const query = db.format(rawString, [email, password]);
+    db.query(query, (err, results) => {
+      if (err) throw err;
+      response.setHeader('id', results.insertId);
+      return response.status(201).json({message: 'usuario cadastrado com sucesso !'});
+    });
+  } catch (error) {
+    return response.status(505).json({error});
+  }
+}
+module.exports.deleteUser = (request, response) => {
+  try {
+    const { userId } = request.params;
+    const rawString = 'delete from usuario where id_user = ?;';
+    const query = db.format(rawString, userId);
+    db.query(query, (err, results) => {
+      if (err) throw err;
+      return response.status(201).json({message: `usuario com id ${userId} deletado com sucesso !`});
+    });
+  } catch (error) {
     return response.status(505).json({error});
   }
 }
--- services/routes.js
+++ services/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { getAllUsers, getUserById } = require('../controller/user');
+const { getAllUsers, getUserById, createUser, deleteUser } = require('../controller/user');
 
 const services = express.Router();
 
 // service to see the status of the API
 services.get('/status',(request, response) => response.send('running'));
-//funcao de get de usuario !
+//rotas de usuario !
 services.get('/user', getAllUsers);
 services.get('/user/:userId', getUserById);
-
+services.post('/user', createUser);
+services.delete('/user/:userId', deleteUser);
 module.exports = services;
