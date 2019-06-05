9af670ed814b25298a527b7c20e82b4dbeaf974a
ArchanjoJr <archanjojr@my.com>
2019-06-04 22:30:31 -0300

    feature: add a new endpoint that returns the userById

--- controller/user.js
+++ controller/user.js
@@ -2,7 +2,7 @@ const db = require('../services/db');
 
 module.exports.getAllUsers = (request, response) => {
   try {
-    const rawString = 'describe usuario';
+    const rawString = 'select * from usuario';
     const query = db.format(rawString);
     db.query(query, (err, results) => {
       if (err) throw err
@@ -10,5 +10,19 @@ module.exports.getAllUsers = (request, response) => {
     });
   } catch (error) {
     return response.status(505).json({error})
+  }
+}
+
+module.exports.getUserById = (request, response) => {
+  try {
+    const rawString = 'select * from usuario where id_user = ?';
+    const { userId } = request.params;
+    const query = db.format(rawString, userId);
+    db.query(query, (err, results) => {
+      if (err) throw err
+      return response.json(results);
+    });
+  } catch(error) {
+    return response.status(505).json({error});
   }
 }
--- services/routes.js
+++ services/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getAllUsers } = require('../controller/user');
+const { getAllUsers, getUserById } = require('../controller/user');
 
 const services = express.Router();
 
 // service to see the status of the API
 services.get('/status',(request, response) => response.send('running'));
+//funcao de get de usuario !
 services.get('/user', getAllUsers);
+services.get('/user/:userId', getUserById);
 
 module.exports = services;
