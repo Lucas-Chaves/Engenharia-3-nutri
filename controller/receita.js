e746f9bf448ded06c021e5d4a7eccf9e5f0bc084
ArchanjoJr <archanjojr@my.com>
2019-06-07 20:27:01 -0300

    feature: update no controller de receita !

--- controller/receita.js
+++ controller/receita.js
@@ -30,11 +30,15 @@ module.exports.getReceitaById = (request, response) => {
 module.exports.createReceita = (request, response) => {
   try {
     const rawString = 'insert into receita(nome, descricao) value (?,?);';
-    const { nome, descricao } = request.body;
+    const { nome, descricao, alimentos } = request.body;
     const query = db.format(rawString, [nome, descricao]);
     db.query(query, (err, results) => {
       if (err) throw err;
       response.setHeader('id', results.insertId);
+      let receitaId = results.insertId;
+      alimentos.forEach(obj => {
+        const newQuery = db.format('insert into ingredientes(id_alimento, id_receita) values ( ?, ?);', [obj, receitaId]);
+      });
       return response.status(201).json({message: 'Receita cadastrado com sucesso !'});
     });
   } catch (error) {
